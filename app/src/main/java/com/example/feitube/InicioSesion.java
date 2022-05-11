d263deb6a7c9022d428a2c73fe46746e4fcd2e50
Fer Guevara <kafera.g10@gmail.com>
2022-05-11 01:38:57 -0500

    Auto stash before merge of "Inciar-Sesion" and "main"

--- app/src/main/java/com/example/feitube/InicioSesion.java
+++ app/src/main/java/com/example/feitube/InicioSesion.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class InicioSesion extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciosesion);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void iniciarSesion(){
+        
     }
 }
---
+++ app/src/main/java/com/example/feitube/model/Usuario.java
@@ -0,0 +1,7 @@
+package com.example.feitube.model;
+
+public class Usuario {
+    private String usuario;
+    private String contraseña;
+    private String programaEducativo;
+}
